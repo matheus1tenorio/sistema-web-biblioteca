67e1bdcf8dd4a4d1f6f7a3a5b07b70eba174e8dd
matheus1tenorio <pc.matheustenorio@gmail.com>
2026-05-04 16:22:20 -0300

    primeiro commit. configurando ambiente

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API da Biblioteca rodando");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
